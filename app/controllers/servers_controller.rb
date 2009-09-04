1c1bb77596f6cb736e6713c25f22dd06ede492fd
Khalid Shaikh <khalid.j.shaikh@gmail.com>
2009-09-03 21:14:21 -0700

    Updated sorting to sort away from now in minutes

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,11 +1,12 @@
 class ServersController < ApplicationController
 
   def index
+    t = Time.now.to_i
+
     groups = Server.find(:all, :select => "ip, response, created_at").group_by(&:ip)
     @ips = {}
     for ip in groups.keys
-      n = 0
-      @ips[ip] = groups[ip].map{|i| n += 1; [n, i.response] }
+      @ips[ip] = groups[ip].sort_by(&:created_at).map{|i| [(t - i.created_at.to_i)/60.0, i.response] }
     end
   end
 
