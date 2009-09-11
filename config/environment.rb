84bd6cccb8c0b6f2ce4dbcc195de45864261a482
Full Chip Regression - Chetan Patil (Generic User) <fcregress@cadstg-3.nvidia.com>
2009-09-10 21:03:34 -0700

    Updated to rails 2.3.4, removed an extra gem

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -40,4 +40,3 @@ Rails::Initializer.run do |config|
   # config.i18n.default_locale = :de
 end
 
-require 'google_chart'
